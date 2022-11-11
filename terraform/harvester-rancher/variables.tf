c83a7f37d5e8107b7fba2c13f516e33af36382cb
Brian Durden <brian.durden@rancherfederal.com>
2022-11-11 16:32:50 -0500

    added harvester rancher

---
+++ terraform/harvester-rancher/variables.tf
@@ -0,0 +1,72 @@
+variable "main_cluster_prefix" {
+    type = string
+    default = "rke2-mgmt-controlplane"
+}
+variable "worker_prefix" {
+    type = string
+    default = "rke2-mgmt-worker"
+}
+variable "kubeconfig_filename" {
+    type = string
+    default = "kube_config_server.yaml"
+}
+variable "cert_manager_version" {
+  type        = string
+  description = "Version of cert-manager to install alongside Rancher (format: 0.0.0)"
+  default     = "1.7.3"
+}
+
+variable "rancher_version" {
+  type        = string
+  description = "Rancher server version (format v0.0.0)"
+  default     = "2.6.7"
+}
+variable "master_vip" {
+    type = string
+    default = "10.10.5.4"
+}
+variable "rancher_server_dns" {
+  type        = string
+  description = "DNS host name of the Rancher server"
+  default = "rancher.home.sienarfleet.systems"
+}
+variable "harbor_url" {
+  type = string
+  default = "harbor.sienarfleet.systems"
+}
+variable "rancher_bootstrap_password" {
+  type = string
+  default = "admin" # TODO: change this once the terraform provider has been updated with the new pw bootstrap logic
+}
+variable "rancher_replicas" {
+  type = string
+  default = 1
+}
+variable "worker_count" {
+  type = string
+  default = 1
+}
+variable "node_disk_size" {
+  type = string
+  default = "20Gi"
+}
+variable "control_plane_ha_mode" {
+  type = bool
+  default = false
+}
+variable "control_plane_cpu_count" {
+  type = string
+  default = 2
+}
+variable "control_plane_memory_size" {
+  type = string
+  default = "4Gi"
+}
+variable "worker_cpu_count" {
+  type = string
+  default = 2
+}
+variable "worker_plane_memory_size" {
+  type = string
+  default = "4Gi"
+}
