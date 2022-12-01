d82717bb8544009e70ae344168d82c5e45bfe0fc
Brian Durden <brian.durden@rancherfederal.com>
2022-12-01 14:51:42 -0500

    updated terraform for harvester 1.1

--- terraform/harvester-infra/networks.tf
+++ terraform/harvester-infra/networks.tf
@@ -1,24 +1,31 @@
+data "harvester_clusternetwork" "mgmt" {
+  name = "mgmt"
+}
 resource "harvester_network" "services" {
   name      = "services"
   namespace = "default"
 
   vlan_id = 5
+  cluster_network_name = data.harvester_clusternetwork.mgmt.name
 }
 resource "harvester_network" "sandbox" {
   name      = "sandbox"
   namespace = "default"
 
   vlan_id = 6
+  cluster_network_name = data.harvester_clusternetwork.mgmt.name
 }
 resource "harvester_network" "dev" {
   name      = "dev"
   namespace = "default"
 
   vlan_id = 7
+  cluster_network_name = data.harvester_clusternetwork.mgmt.name
 }
 resource "harvester_network" "prod" {
   name      = "prod"
   namespace = "default"
 
   vlan_id = 8
+  cluster_network_name = data.harvester_clusternetwork.mgmt.name
 }
